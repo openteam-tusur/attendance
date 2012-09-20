7c09bc44b872744ef4432e2c61dca50d1e2271bd
Manin Iliy <bkmzvf@gmail.com>
2012-09-20 11:08:29 +0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+group :assets do
+  gem 'coffee-rails'
+  gem 'compass-rails'
+  gem 'sass-rails'
+  gem 'therubyracer'
+  gem 'uglifier'
+end
+
+group :default do
+  gem 'jquery-rails'
+  gem 'inherited_resources'
+  gem 'pg'
+  gem 'rails'
+  gem 'russian'
+  gem 'simple_form'
+  gem 'sqlite3'
+end
+
+group :development do
+  gem 'hirb', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attendance::Application.config.secret_token = 'e450051ff438981dc1d8b252af0549ea77148719ad16827c48141119b93c00298cf497564cc7201d91d1ce6f295ec345559a65b504f9fa6c181e28107d6de2b0'
