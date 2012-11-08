8a95aec3eb20a5e7bc58b864dd0755c3a2220d04
Manin Iliy <bkmzvf@gmail.com>
2012-11-08 14:00:11 +0700

    dependent destroy

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,8 +8,8 @@ class Lesson < ActiveRecord::Base
   belongs_to :discipline
   belongs_to :group
   belongs_to :lecturer
-  has_many :realizes
-  has_many :presences
+  has_many :realizes, :dependent => :destroy
+  has_many :presences, :dependent => :destroy
   has_many :lecturers, :through => :realizes
 
   enumerize :kind, :in => [:lecture, :practice, :laboratory, :research, :design]
