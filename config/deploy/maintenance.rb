38c801b44493d211ea6aed66899cfc837efde9bf
tab <tukhfatullin@gmail.com>
2012-11-09 14:31:54 +0700

    Maintenance only for manage

--- config/deploy/maintenance.rb
+++ config/deploy/maintenance.rb
@@ -2,7 +2,7 @@ namespace :deploy do
   namespace :web do
     task :disable, :roles => :web, :except => { :no_release => true } do
       require 'erb'
-      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      on_rollback { run "rm #{shared_path}/system/manage/maintenance.html" }
 
       reason = ENV['REASON']
       deadline = ENV['UNTIL']
@@ -10,7 +10,11 @@ namespace :deploy do
       template = File.read("./app/views/layouts/maintenance.html.erb")
       result = ERB.new(template).result(binding)
 
-      put result, "#{shared_path}/system/maintenance.html", :mode => 0644
+      put result, "#{shared_path}/system/manage/maintenance.html", :mode => 0644
+    end
+
+    task :enable, :roles => :web, :except => { :no_release => true } do
+      run "rm -f #{shared_path}/system/manage/maintenance.html"
     end
   end
 end
