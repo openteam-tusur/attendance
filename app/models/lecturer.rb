9fbfe9e7ab48a0b4e303c8c0ec82e48ce149ce46
Manin Iliy <bkmzvf@gmail.com>
2012-12-12 14:31:42 +0700

    статистика преподов

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -12,4 +12,8 @@ class Lecturer < Person
     res << "#{patronymic[0]}." if patronymic?
     res.join(' ')
   end
+
+  def losed_lessons
+    lessons.where("lessons.state = 'wasnt_took_place' AND lessons.date_on BETWEEN ? AND ?", Presence.last_week_begin, Presence.last_week_end)
+  end
 end
