913762f87a24eb24265e1c0c5cbe4ecdbd409a78
tab <tukhfatullin@gmail.com>
2013-02-18 16:43:52 +0700

    Fix whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,18 +5,17 @@ dir = Directories.new
 if RUBY_PLATFORM =~ /freebsd/
   set :job_template, "/usr/local/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
 else
-#  set :job_template, "/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
-  set :job_template, "bash -l -c ':job'"
+  set :job_template, "bin/bash -lic ':job'"
 end
 
 every :month do
-  rake 'sync:f_and_g', :output => { :error => 'error-fg.log', :standard => 'cron-fg.log'}
+  rake 'sync:f_and_g', :output => { :error => 'log/error-fg.log', :standard => 'log/cron-fg.log'}
 end
 
 every :sunday, :at => '1am' do
-  rake 'sync:students', :output => { :error => 'error-students.log', :standard => 'cron-students.log'}
+  rake 'sync:students', :output => { :error => 'log/error-students.log', :standard => 'log/cron-students.log'}
 end
 
 every :day, :at => '4am' do
-  rake 'sync:lessons', :output => { :error => 'error-lessons.log', :standard => 'cron-lessons.log'}
+  rake 'sync:lessons', :output => { :error => 'log/error-lessons.log', :standard => 'log/cron-lessons.log'}
 end
