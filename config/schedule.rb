f236d50f67bc31f0522bdbf4138600e7fcd25e74
tab <tukhfatullin@gmail.com>
2013-02-18 16:52:31 +0700

    Fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@ dir = Directories.new
 if RUBY_PLATFORM =~ /freebsd/
   set :job_template, "/usr/local/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
 else
-  set :job_template, "bin/bash -lic ':job'"
+  set :job_template, "/bin/bash -lic ':job'"
 end
 
 every :month do
