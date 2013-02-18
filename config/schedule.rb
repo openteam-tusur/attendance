0490e0a4f6cb6ebb28a80be352dfb9bbac24ab4b
tab <tukhfatullin@gmail.com>
2013-02-18 15:30:09 +0700

    Update whenever gem

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,8 @@ dir = Directories.new
 if RUBY_PLATFORM =~ /freebsd/
   set :job_template, "/usr/local/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
 else
-  set :job_template, "/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
+#  set :job_template, "/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
+  set :job_template, "bash -l -c ':job'"
 end
 
 every :month do
