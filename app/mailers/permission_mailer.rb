a5601d39006f280223e943bf2adeb743a742c1f3
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-07-18 10:27:26 +0700

    default from for mailer

--- app/mailers/permission_mailer.rb
+++ app/mailers/permission_mailer.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 class PermissionMailer < ActionMailer::Base
-  default :from => Settings['app.mail_from']
-
   def invitation_email(permission)
     @permission = permission
 
-    mail :to => @permission.email, :subject => 'Электронный журнал посещаемости занятий'
+    mail :to => @permission.email,
+      :subject => 'Электронный журнал посещаемости занятий',
+      :from => Settings['app.mail_from']
   end
 end
