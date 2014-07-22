ef740cb4adfdaf432dd31362ab72e1cd18df462c
tab <tukhfatullin@gmail.com>
2014-07-22 16:44:48 +0700

    Add tusur_header with redis-session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Attendance::Application.config.session_store :cookie_store, key: '_attendance_session'
+Attendance::Application.config.session_store :redis_store, :servers => Settings['session_store.url'], :domain => Settings['app.domain']
