25fbe4e6cd16e7d9ffd0bff18a0a194061e07a94
Manin Iliy <bkmzvf@gmail.com>
2014-07-24 14:29:16 +0700

    render navigation if user signed in

--- config/namespaces_navigation.rb
+++ config/namespaces_navigation.rb
@@ -5,7 +5,7 @@ SimpleNavigation::Configuration.run do |navigation|
     Permission.available_roles.each do |role|
       primary.item role.to_sym, I18n.t("role_names.#{role}"), [role.to_sym, :root] if current_user.send("#{role}?")
     end
-  end
+  end if user_signed_in?
 end
 
 SimpleNavigation.register_renderer :first_renderer  => FirstRenderer
