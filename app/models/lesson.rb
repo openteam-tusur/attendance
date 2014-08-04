1979ae2690b6ad5406c8688d2daa4f3034544e3f
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-04 16:30:35 +0700

    groups index for curator

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ActiveRecord::Base
+  include Semester
+
   belongs_to :group
   belongs_to :discipline
   has_many   :presences,  :dependent  => :destroy
@@ -10,4 +12,5 @@ class Lesson < ActiveRecord::Base
   scope :actual,      ->       { where(:deleted_at => nil) }
   scope :not_actual,  ->       { where.not(:deleted_at => nil) }
   scope :unfilled,    ->       { joins(:presences).where(:presences => { :state => nil }).uniq }
+  scope :by_semester,   ->     { where('date_on > :start_at and date_on < :end_at', :start_at => semester_starts_at, :end_at => Date.today.prev_week.end_of_week)}
 end
