85f22970f6c77a356cf230ae7a13cfdd648c2289
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-06 16:09:54 +0700

    create and update student misses

--- app/controllers/dean/misses_controller.rb
+++ app/controllers/dean/misses_controller.rb
@@ -1,4 +1,10 @@
 class Dean::MissesController < AuthController
-  def index
+  has_scope :for_student, :default => 1
+  actions :all, :except => [:show, :edit]
+
+  private
+
+  def miss_params
+    params.require(:miss).permit(:missing_id, :starts_at, :ends_at, :note)
   end
 end
--- app/models/miss.rb
+++ app/models/miss.rb
@@ -1,3 +1,7 @@
 class Miss < ActiveRecord::Base
+  attr_accessor :name
+
   belongs_to :missing, :polymorphic => true
+
+  scope :for_student, ->(_) { where(:missing_type => 'Student') }
 end
