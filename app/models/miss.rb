8e0c68c3fd7cccd8d0f0390f0ae521264b609dff
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-06 17:06:34 +0700

    students miss list on index

--- app/models/miss.rb
+++ app/models/miss.rb
@@ -4,4 +4,8 @@ class Miss < ActiveRecord::Base
   belongs_to :missing, :polymorphic => true
 
   scope :for_student, ->(_) { where(:missing_type => 'Student') }
+
+  def absent_period
+    "с #{I18n.l(self.starts_at)} по #{I18n.l(self.ends_at)}"
+  end
 end
