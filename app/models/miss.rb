69f7fe5bb2b2c744ec0c346da31f9d83c512364c
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-07 16:11:13 +0700

    dean: disrupted lesons grouped by lecturers

--- app/models/miss.rb
+++ app/models/miss.rb
@@ -4,6 +4,7 @@ class Miss < ActiveRecord::Base
   belongs_to :missing, :polymorphic => true
 
   scope :for_missing, ->(type) { where(:missing_type => type) }
+  scope :by_date,     ->(date) { where('starts_at <= :date and ends_at >= :date', :date => date) }
 
   def absent_period
     "с #{I18n.l(self.starts_at)} по #{I18n.l(self.ends_at)}"
--- app/models/realize.rb
+++ app/models/realize.rb
@@ -3,8 +3,16 @@ class Realize < ActiveRecord::Base
   belongs_to :lesson
   after_initialize :set_state
 
+  scope :wasnt, -> { where(:state => :wasnt) }
+  scope :ordered, -> { joins(:lecturer).joins(:lesson).order('people.surname, lessons.date_on desc')}
+  scope :with_lessons, -> { includes(:lesson) }
+
   def to_s
     I18n.t("states.realizes.#{state}")
+  end
+
+  def missed_by_cause?
+    self.lecturer.misses.by_date(self.lesson.lesson_time).any?
   end
 
   private
