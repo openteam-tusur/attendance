936bd507ebc1b0a7a339a940e8ed209fbf03a0c7
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-08 11:28:26 +0700

    show only dean faculty disruptions

--- app/controllers/dean/disruptions_controller.rb
+++ app/controllers/dean/disruptions_controller.rb
@@ -2,6 +2,7 @@ class Dean::DisruptionsController < AuthController
   actions :index
 
   def index
-      @disruptions = Realize.wasnt.with_lessons.ordered.group_by(&:lecturer)
+    @faculty = current_user.faculties.first
+    @disruptions = @faculty.realizes.wasnt.with_lessons.ordered.group_by(&:lecturer)
   end
 end
