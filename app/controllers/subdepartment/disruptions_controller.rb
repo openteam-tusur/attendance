e11bab22d8d5595598050a2d661f4c397ad7bb9b
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-11 10:18:36 +0700

    subdepartment: disrupted lesons grouped by lecturers

--- app/controllers/subdepartment/disruptions_controller.rb
+++ app/controllers/subdepartment/disruptions_controller.rb
@@ -1,4 +1,8 @@
 class Subdepartment::DisruptionsController < AuthController
+  actions :index
+
   def index
+    @subdepartment = current_user.subdepartments.first
+    @disruptions = @subdepartment.realizes.wasnt.with_lessons.ordered.group_by(&:lecturer)
   end
 end
