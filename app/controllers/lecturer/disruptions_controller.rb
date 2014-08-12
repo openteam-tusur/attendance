bb70cc2bb91abb32c5bec633b370cf6a71ac3df7
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-12 15:21:40 +0700

    lecturer disruptions

--- app/controllers/lecturer/disruptions_controller.rb
+++ app/controllers/lecturer/disruptions_controller.rb
@@ -1,4 +1,6 @@
 class Lecturer::DisruptionsController < AuthController
   def index
+    @lecturer = current_user.lecturers.first
+    @disruptions = @lecturer.realizes.wasnt.with_lessons.ordered_by_lesson
   end
 end
