bbeb22c4469620c98af42fa823f91c8b1422ed82
tab <tukhfatullin@gmail.com>
2014-08-12 08:45:15 +0700

    Верстка формы добавления роли

--- config/administrator_navigation.rb
+++
@@ -1,7 +0,0 @@
-SimpleNavigation::Configuration.run do |navigation|
-  navigation.items do |primary|
-    primary.item :permissions,  I18n.t('page_title.permissions.index'),   administrator_permissions_path(:for_role => :administrator)
-    primary.item :sync,         I18n.t('page_title.syncs.index'),         administrator_syncs_path
-    primary.item :sidekiq,     'Sidekiq',                                 administrator_sidekiq_path
-  end
-end
