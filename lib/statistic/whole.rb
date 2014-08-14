10ba648760096705a56683c0dea3a96f347efc2f
Manin Iliy <bkmzvf@gmail.com>
2014-08-14 17:05:10 +0700

    Whole statistic

--- lib/statistic/whole.rb
+++ lib/statistic/whole.rb
@@ -1,6 +1,13 @@
 class Statistic::Whole < Statistic::Base
-  # Посещаемость по университету
+  def uniq_id
+    @uniq_id ||= "university"
+  end
+
   def attendance_by_faculties
-    {}
+    res = {}
+    Faculty.actual.each do |faculty|
+      res.merge! faculty.abbr => Statistic::Faculty.new(faculty).total_attendance
+    end
+    res
   end
 end
