c076bc8a8bb2f4d0175b10db0bea78ee8822af42
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-15 15:15:36 +0700

    dean misses ordering and pagination

--- app/controllers/dean/misses_controller.rb
+++ app/controllers/dean/misses_controller.rb
@@ -2,6 +2,13 @@ class Dean::MissesController < AuthController
   actions :all, :except => :show
 
   has_scope :for_missing, :default => 'Student'
+  has_scope :ordered, :default => 1, :only => :index
+
+  def index
+    index!{
+      @misses = Kaminari.paginate_array(@misses).page(params[:page]).per(10)
+    }
+  end
 
   private
 
