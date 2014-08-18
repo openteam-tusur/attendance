b87bba6b6674cc024027bcd2fc07d9b58533d33d
Manin Iliy <bkmzvf@gmail.com>
2014-08-18 16:03:12 +0700

    FIX f*cking ugly bug with Struct. #wtf

--- app/models/group_leader.rb
+++ app/models/group_leader.rb
@@ -1,4 +1,5 @@
-class GroupLeader < Struct.new(:faculty)
+class GroupLeader
+  attr_accessor :faculty
   def who_unfilled
     faculty.groups.joins(:group_leaders).select{ |g| g.absent_days > 0 }.map{|g| [g.number, g.group_leaders.first, g.absent_days]}.sort_by{|item| item.first}
   end
