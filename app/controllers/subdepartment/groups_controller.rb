9ea878334a3f4abaf943f1b38265474580a8b3e4
tab <tukhfatullin@gmail.com>
2014-08-19 16:30:23 +0700

    Еще немного фильтров для статистики

--- app/controllers/subdepartment/groups_controller.rb
+++ app/controllers/subdepartment/groups_controller.rb
@@ -1,7 +1,12 @@
 class Subdepartment::GroupsController < AuthController
+  include FilterParams
+  include DateRange
+
   def index
     @subdepartment = current_user.subdepartments.first
     @groups        = @subdepartment.groups
+    @attendance_by_date = Statistic::Subdepartment.new(@subdepartment).attendance_by_date(**filter_params)
+    @attendance_by_groups = Statistic::Subdepartment.new(@subdepartment).attendance_by_groups(**filter_params)
   end
 
   def show
