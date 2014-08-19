117060aa0905eafb4e3d58c334d0169bd15a536b
tab <tukhfatullin@gmail.com>
2014-08-19 09:21:47 +0700

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,7 +32,7 @@ module ApplicationHelper
   end
 
   def calendar_months(date)
-    if date.month > 6
+    if date.month >= 6
       ((date.beginning_of_year + 6.month)..date.end_of_year).group_by(&:month)
     else
       (date.beginning_of_year..(date.end_of_year - 6.month)).group_by(&:month)
