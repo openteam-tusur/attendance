966dfff7a8adf9f03b7ab3e1d17c8157af9d983e
tab <tukhfatullin@gmail.com>
2014-08-19 14:28:13 +0700

    Фильтры для статистики посещаемости факультета

--- lib/statistic/faculty.rb
+++ lib/statistic/faculty.rb
@@ -3,10 +3,10 @@ class Statistic::Faculty < Statistic::Base
     @uniq_id ||= "faculty:#{context.abbr}"
   end
 
-  def attendance_by_groups
+  def attendance_by_groups(from: nil, to: nil)
     res = {}
     context.groups.each do |group|
-      res.merge! group.number => Statistic::Group.new(group).total_attendance
+      res.merge! group.number => Statistic::Group.new(group).total_attendance(from: from, to: to)
     end
     res
   end
