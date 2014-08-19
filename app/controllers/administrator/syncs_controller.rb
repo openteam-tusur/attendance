f65ad08f6abb6d754d7886146e2389947fb51dfd
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-19 12:51:14 +0700

    such paginate

--- app/controllers/administrator/syncs_controller.rb
+++ app/controllers/administrator/syncs_controller.rb
@@ -1,6 +1,10 @@
 class Administrator::SyncsController < AuthController
   actions :index
 
+  def index
+    index!{ @syncs = Kaminari.paginate_array(@syncs).page(params[:page]).per(10) }
+  end
+
   has_scope :by_created_at, :default => 1 do |controller, scope, _|
     scope.order('created_at DESC')
   end
