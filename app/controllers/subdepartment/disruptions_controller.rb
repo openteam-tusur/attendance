6d94ebdb00fe126cf979db127fe6f6b034f61648
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-19 14:34:30 +0700

    remove pagination for dean and subdepartment disruptions

--- app/controllers/subdepartment/disruptions_controller.rb
+++ app/controllers/subdepartment/disruptions_controller.rb
@@ -3,6 +3,6 @@ class Subdepartment::DisruptionsController < AuthController
 
   def index
     @subdepartment = current_user.subdepartments.first
-    @disruptions = Kaminari.paginate_array(@subdepartment.realizes.wasnt.with_lessons.ordered_by_lecturer.group_by(&:lecturer)).page(params[:page]).per(10)
+    @disruptions = @subdepartment.realizes.wasnt.with_lessons.ordered_by_lecturer.group_by(&:lecturer)
   end
 end
