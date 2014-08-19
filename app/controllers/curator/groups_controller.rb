c7579a05e126131cc49fad37ea3a65919c8a7383
tab <tukhfatullin@gmail.com>
2014-08-19 14:15:53 +0700

    Фильтры для статистики посещаемости группы для куратора

--- app/controllers/curator/groups_controller.rb
+++ app/controllers/curator/groups_controller.rb
@@ -1,8 +1,12 @@
 class Curator::GroupsController < AuthController
+  include FilterParams
+  include DateRange
   actions :index, :show
 
   def show
     @group = current_user.curated_groups.find(params[:id])
+    @attendance_by_date = Statistic::Group.new(@group).attendance_by_date(**filter_params)
+    @attendance_by_students = Statistic::Group.new(@group).attendance_by_students(**filter_params)
   end
 
   def index
