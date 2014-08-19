34b0c79eb80e6302f90ab415a368d8a917dfd21c
tab <tukhfatullin@gmail.com>
2014-08-19 14:28:13 +0700

    Фильтры для статистики посещаемости факультета

--- app/controllers/dean/groups_controller.rb
+++ app/controllers/dean/groups_controller.rb
@@ -1,6 +1,12 @@
 class Dean::GroupsController < AuthController
+  include FilterParams
+  include DateRange
+  actions :index
+
   def index
     @faculty = current_user.faculties.first
     @groups  = current_user.faculty_groups
+    @attendance_by_date = Statistic::Faculty.new(@faculty).attendance_by_date(**filter_params)
+    @attendance_by_groups = Statistic::Faculty.new(@faculty).attendance_by_groups(**filter_params)
   end
 end
