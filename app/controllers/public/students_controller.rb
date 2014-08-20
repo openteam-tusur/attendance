fa19329b35703456468f98701fbcddb72c8bc920
tab <tukhfatullin@gmail.com>
2014-08-20 15:27:33 +0700

    Фильтр дат на просмотре студента

--- app/controllers/public/students_controller.rb
+++ app/controllers/public/students_controller.rb
@@ -1,6 +1,9 @@
 class Public::StudentsController < ApplicationController
+  include FilterParams
+  include DateRange
+
   def show
     @student = Student.find_by(:secure_id => params[:id])
-    @omissions = @student.presences.by_state(:wasnt)
+    @omissions = @student.presences.by_state(:wasnt).between_dates(*filter_params.values)
   end
 end
