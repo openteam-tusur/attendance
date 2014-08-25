a4bd351bbf5f51d0bbaa2aac3d1e1629c4742f81
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-25 10:38:10 +0700

    prepare subdepartment disruptions controller for smart filter

--- app/controllers/subdepartment/disruptions_controller.rb
+++ app/controllers/subdepartment/disruptions_controller.rb
@@ -9,6 +9,17 @@ class Subdepartment::DisruptionsController < AuthController
 
   def index
     @subdepartment = current_user.subdepartments.first
-    @disruptions = @subdepartment.realizes.wasnt.between_dates(*filter_params.values).group_by(&:lecturer)
+    @disruptions = Sunspot.search Realize do |s|
+      s.with(:lecturer).starting_with(params[:name]) if params[:name].present?
+      s.with(:lesson_date).between(filter_params[:from]..filter_params[:to])
+      s.with :approved, params[:approved] == 1 ? true : false unless params[:approved].nil?
+      s.with(:subdepartment_id, @subdepartment.id)
+      s.with :state, :wasnt
+
+      s.order_by(:lesson_date)
+      s.group(:lecturer) { limit 10000 }
+
+      s.paginate :page => params[:page], :per_page => 10
+    end.group(:lecturer).groups
   end
 end
