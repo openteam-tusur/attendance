bf0f8110b71463d4b7d940445323558786ee894e
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-26 12:43:42 +0700

    dean group_leaders pagination

--- app/controllers/dean/group_leaders_controller.rb
+++ app/controllers/dean/group_leaders_controller.rb
@@ -4,6 +4,6 @@ class Dean::GroupLeadersController < AuthController
 
   def index
     @faculty = current_user.faculties.first
-    @group_leaders = GroupLeader.new(@faculty).who_unfilled
+    @group_leaders = Kaminari.paginate_array(GroupLeader.new(@faculty).who_unfilled).page(params[:page]).per(10)
   end
 end
