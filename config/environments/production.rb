1edd99df7fb782278df34c9b45c7ccaad7a7a2ca
Ivan Puchkov <rake.eclipse@gmail.com>
2014-08-26 10:09:10 +0700

    fucking assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Attendance::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif administrator.js curator.js dean.js education_department.js group_leader.js lecturer.js public.js subdepartment.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
