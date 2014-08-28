98583f0a0de12373be4952098c7d3f73d196f2a3
Manin Iliy <bkmzvf@gmail.com>
2014-08-28 09:58:12 +0700

    dont click on student

--- app/controllers/education_department/groups_controller.rb
+++ app/controllers/education_department/groups_controller.rb
@@ -5,7 +5,7 @@ class EducationDepartment::GroupsController < AuthController
   def show
     @charts = {}
     @group = Group.actual.find_by(:number => params[:id])
-    group_statistic = Statistic::Group.new(@group, current_namespace)
+    group_statistic = Statistic::Group.new(@group, nil)
     @charts['attendance_by_dates.line']   = group_statistic.attendance_by_date(**filter_params)
     @charts['attendance_by_students.bar'] = group_statistic.attendance_by('students', **filter_params)
   end
