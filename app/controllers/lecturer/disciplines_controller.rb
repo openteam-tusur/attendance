8473634d61de9528bd30baf11bb0f5adab5ea15a
tab <tukhfatullin@gmail.com>
2014-08-29 16:57:13 +0700

    lecturer stats fixed

--- app/controllers/lecturer/disciplines_controller.rb
+++ app/controllers/lecturer/disciplines_controller.rb
@@ -2,13 +2,25 @@ class Lecturer::DisciplinesController < AuthController
   include FilterParams
   include DateRange
 
+  before_filter :find_lecturer
+
+  def index
+    @charts = {}
+    lecturer_statistic = Statistic::Lecturer.new(@lecturer, current_namespace)
+    @charts['attendance_by_disciplines.bar'] = lecturer_statistic.attendance_by('disciplines', **filter_params)
+  end
 
   def show
-    @charts = {}
+    @charts     = {}
     @discipline = params[:id]
-    @lecturer = current_user.lecturers.first
     @parent_url = lecturer_root_path(:filter => params[:filter])
-    @charts['attendance_by_dates.line'] = Statistic::Lecturer.new(@lecturer, nil).attendance_by_date_of_kind('disciplines', @discipline, **filter_params)
-    @charts['attendance_by_groups.bar'] = Statistic::Discipline.new("#{@discipline}_#{@lecturer}", "#{current_namespace}/disciplines/#{@discipline}").attendance_by('groups', **filter_params)
+    lecturer_statistic = Statistic::Lecturer.new("#{@lecturer}:#{@discipline}", "#{current_namespace}/disciplines/#{@discipline}")
+    @charts['attendance_by_dates.line'] = lecturer_statistic.attendance_by_date(**filter_params)
+    @charts['attendance_by_groups.bar'] = lecturer_statistic.attendance_by('groups', **filter_params)
+  end
+
+  private
+  def find_lecturer
+    @lecturer = current_user.lecturers.first
   end
 end
