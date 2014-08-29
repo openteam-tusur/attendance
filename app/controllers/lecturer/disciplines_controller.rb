6dde990c2b3a731901841c59ef3518fd90f0f0ea
Manin Iliy <bkmzvf@gmail.com>
2014-08-29 09:48:57 +0700

    link to back for lecturer namespace

--- app/controllers/lecturer/disciplines_controller.rb
+++ app/controllers/lecturer/disciplines_controller.rb
@@ -7,6 +7,7 @@ class Lecturer::DisciplinesController < AuthController
     @charts = {}
     @discipline = params[:id]
     @lecturer = current_user.lecturers.first
+    @parent_url = lecturer_root_path(:filter => params[:filter])
     @charts['attendance_by_dates.line'] = Statistic::Lecturer.new(@lecturer, nil).attendance_by_date_of_kind('disciplines', @discipline, **filter_params)
     @charts['attendance_by_groups.bar'] = Statistic::Discipline.new("#{@discipline}_#{@lecturer}", "#{current_namespace}/disciplines/#{@discipline}").attendance_by('groups', **filter_params)
   end
