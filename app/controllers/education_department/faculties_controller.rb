777821745d839671ef2508b653198489e028df99
tab <tukhfatullin@gmail.com>
2014-08-30 13:32:20 +0700

    Fix student statistic for education_department namespace

--- app/controllers/education_department/faculties_controller.rb
+++ app/controllers/education_department/faculties_controller.rb
@@ -18,13 +18,20 @@ class EducationDepartment::FacultiesController < AuthController
   def show
     @charts = {}
     @faculty = Faculty.actual.find_by(:abbr => params[:id])
+    @course = params[:course_id]
 
-    if params[:course_id]
-      faculty_statistic = Statistic::Faculty.new("#{@faculty}:#{params[:course_id]}", "#{current_namespace}/courses/#{params[:course_id]}/faculties/#{@faculty}")
+    if @course
+      faculty_statistic = Statistic::Faculty.new("#{@faculty}:#{@course}", "#{current_namespace}/courses/#{@course}/faculties/#{@faculty}")
+
+      @parent_url = education_department_course_path(@course, :filter => params[:filter])
+
       @charts['attendance_by_dates.line']          = faculty_statistic.attendance_by_date(**filter_params)
       @charts['attendance_by_groups.bar']          = faculty_statistic.attendance_by('groups', **filter_params)
     else
       faculty_statistic = Statistic::Faculty.new(@faculty, "#{current_namespace}/faculties/#{@faculty}")
+
+      @parent_url = education_department_faculties_path(:filter => params[:filter])
+
       @charts['attendance_by_dates.line']          = faculty_statistic.attendance_by_date(**filter_params)
       @charts['attendance_by_courses.bar']         = faculty_statistic.attendance_by('courses', **filter_params)
     end
