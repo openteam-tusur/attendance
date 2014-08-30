10402eaf88b2cdf21ee224f320b008522810a00d
tab <tukhfatullin@gmail.com>
2014-08-30 13:32:20 +0700

    Fix student statistic for education_department namespace

--- app/controllers/education_department/courses_controller.rb
+++ app/controllers/education_department/courses_controller.rb
@@ -6,6 +6,12 @@ class EducationDepartment::CoursesController < AuthController
     @charts = {}
     @course = params[:id]
 
+    if params.to_a[-2..-2][0][0] == 'faculty_id'
+      @parent_url = education_department_faculty_path(params[:faculty_id], :filter => params[:filter])
+    else
+      @parent_url = education_department_faculties_path(:filter => params[:filter])
+    end
+
     if params[:faculty_id]
       faculty_statistic = Statistic::Faculty.new("#{params[:faculty_id]}:#{@course}", "#{current_namespace}/faculties/#{params[:faculty_id]}/courses/#{@course}")
       @charts['attendance_by_dates.line']          = faculty_statistic.attendance_by_date(**filter_params)
