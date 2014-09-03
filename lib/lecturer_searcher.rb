97211d8ff32da2000689f8a43452a63ebcf2da58
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-03 10:50:41 +0700

    remove lecturer search

--- lib/lecturer_searcher.rb
+++
@@ -1,8 +0,0 @@
-class LecturerSearcher < Struct.new(:params)
-  def autocomplete_search
-    Sunspot.search Lecturer do |q|
-      q.with(:info).starting_with(params[:term])
-      q.with(:deleted_at, nil)
-    end
-  end
-end
