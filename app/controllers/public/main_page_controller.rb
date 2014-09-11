85142156edf3ee4d9a61e974740c1cb497f091a3
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-11 15:43:37 +0700

    redirect to namespace after sign in

--- app/controllers/public/main_page_controller.rb
+++ app/controllers/public/main_page_controller.rb
@@ -1,4 +1,13 @@
 class Public::MainPageController < ApplicationController
   def index
+    if user_signed_in? && available_user_namespaces.any?
+      redirect_to [current_user.permissions.first.role.to_sym, :root]
+    end
+  end
+
+  private
+
+  def available_user_namespaces
+    @available_user_namespaces ||= current_user.permissions.pluck(:role)
   end
 end
