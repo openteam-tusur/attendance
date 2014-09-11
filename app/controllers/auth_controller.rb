bd41911ea8554a03f3a7806b7c31877a3d63466a
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-11 15:43:37 +0700

    redirect to namespace after sign in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,5 @@
 class AuthController < ApplicationController
   include ApplicationHelper
-  before_filter        :redirect_to_namespace
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
@@ -8,16 +7,5 @@ class AuthController < ApplicationController
 
   def current_ability
     Ability.new(current_user, current_namespace)
-  end
-
-  private
-  def redirect_to_namespace
-    return if !user_signed_in? || available_user_namespaces.empty?
-    return if available_user_namespaces.include?(current_namespace.to_s)
-    redirect_to [current_user.permissions.first.role.to_sym, :root]
-  end
-
-  def available_user_namespaces
-    @available_user_namespaces ||= current_user.permissions.pluck(:role)
   end
 end
