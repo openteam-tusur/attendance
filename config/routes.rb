44b3ee4d0559c140fd1c01949ce20576e44dc504
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-22 15:38:11 +0700

    remove sso-auth gem, remove devise, remove user model; add auth-client gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 require 'sidekiq/web'
 Attendance::Application.routes.draw do
-  devise_for :users, :path => 'auth', :controllers => {:omniauth_callbacks => 'sso/auth/omniauth_callbacks'}, :skip => [:sessions]
-
-  devise_scope :users do
-    get 'sign_out' => 'sso/auth/sessions#destroy', :as => :destroy_user_session
-    get 'sign_in' => redirect('/auth/auth/identity'), :as => :new_user_session
-  end
-
   scope :module => :public do
     get '/search' => 'search#index'
     get '/students/:id' => 'students#show', :as => :student
