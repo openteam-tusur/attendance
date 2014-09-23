d3452f676725f66934c6575dacbf5e1b6f748fd2
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-23 15:09:57 +0700

    turn back old user model; fix permission migration; some fixes

--- config/initializers/redis_user_connector.rb
+++ config/initializers/redis_user_connector.rb
@@ -1,3 +1,3 @@
 RedisUserConnector.connect :url => Settings['redis_user_connector.url']
 
-require Rails.root.join('app', 'models', 'user.rb')
+require Rails.root.join('app', 'models', 'auth_client', 'user.rb')
--- db/migrate/20140922074522_add_old_user_id_to_permission.rb
+++ db/migrate/20140922074522_add_old_user_id_to_permission.rb
@@ -1,14 +1,23 @@
 class AddOldUserIdToPermission < ActiveRecord::Migration
   def up
+    add_column :permissions, :old_user_uid, :integer
     add_column :permissions, :old_user_id, :integer
-    Permission.find_each do |perm|
+
+    Permission.where.not(:user_id => nil).each do |perm|
+      user = User.find(perm.user_id)
+
+      perm.update_column(:old_user_uid, user.uid)
       perm.update_column(:old_user_id, perm.user_id)
     end
+
+    Permission.update_all(:user_id => nil)
 
     change_column :permissions, :user_id, :string
   end
 
   def down
+    Permission.update_all(:user_id => nil)
+
     change_column :permissions, :user_id, :integer
 
     Permission.find_each do |perm|
