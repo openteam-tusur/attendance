561c8b925c39f9c98f120ec47f2fa8d4377205a3
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-24 11:47:00 +0700

    show inactive and absent group_leaders for dean

--- app/controllers/dean/group_leaders_controller.rb
+++ app/controllers/dean/group_leaders_controller.rb
@@ -3,6 +3,8 @@ class Dean::GroupLeadersController < AuthController
 
   def index
     @faculty = current_user.faculties.first
+    @without_group_leaders = @faculty.groups.select {|g| g.permissions.where(:role => :group_leader).empty?}
+    @inactive = @faculty.groups.joins(:permissions).where(:permissions => {:role => :group_leader, :user_id => nil})
     @group_leaders = Kaminari.paginate_array(GroupLeader.new(@faculty).who_unfilled).page(params[:page]).per(10)
   end
 end
