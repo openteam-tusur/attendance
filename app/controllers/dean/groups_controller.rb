f9889c6620636267b89b2e87dd6dc19db629acda
Ivan Puchkov <rake.eclipse@gmail.com>
2014-10-23 16:52:16 +0700

    dean lecturers statistics

--- app/controllers/dean/groups_controller.rb
+++ app/controllers/dean/groups_controller.rb
@@ -25,10 +25,15 @@ class Dean::GroupsController < AuthController
     @group = @faculty.groups.actual.find_by(:number => params[:id])
     @course = params[:course_id]
     @subdepartment = params[:subdepartment_id]
+    @discipline = params[:discipline_id]
+    @lecturer = params[:lecturer_id]
 
-    if params.to_a[-1][0] == 'id' && params.to_a[-2..-1][0][0] != 'subdepartment_id' && params.to_a[-2..-1][0][0] != 'course_id'
+    if params.to_a[-1][0] == 'id' && params.to_a[-2..-1][0][0] != 'subdepartment_id' && params.to_a[-2..-1][0][0] != 'course_id' && params.to_a[-2..-1][0][0] != 'discipline_id'
       @parent_url = list_dean_groups_path
       group_statistic = Statistic::Group.new(@group, "#{current_namespace}/groups/#{@group}")
+    elsif params.to_a[-2..-1][0][0] == 'discipline_id'
+      @parent_url = dean_lecturer_discipline_path(@lecturer, @discipline, :filter => params[:filter])
+      group_statistic = Statistic::Lecturer.new("#{@lecturer}:#{@faculty}:#{@discipline}:#{@group}", nil)
     elsif params.to_a[-3..-2][0][0] == 'course_id'
       @parent_url = dean_course_subdepartment_path(@course, @subdepartment, :filter => params[:filter])
       group_statistic = Statistic::Group.new(@group, "#{current_namespace}/courses/#{@course}/subdepartments/#{@subdepartment}/groups/#{@group}")
