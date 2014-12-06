2daa880f4c835eef2e939ba3db1b74e73449b600
tab <tab@users.noreply.github.com>
2014-12-06 10:32:20 +0600

    Disable statistic task

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,7 @@ every :day, :at => '4:00am' do
   rake 'sync:lessons'
 end
 
-every 1.hours do
-  rake 'statistic:calculate'
-end
+# TODO: Statistic task
+# every 1.hours do
+#  rake 'statistic:calculate'
+# end
