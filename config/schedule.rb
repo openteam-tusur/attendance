0a3ded77b9d82a3040b8b370b14d40d199514af4
Ivan Puchkov <rake.eclipse@gmail.com>
2015-01-23 09:16:49 +0600

    remove statistic calculation from cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,3 @@ set :job_template, "bash -c 'source ~/.rvm/environments/default && :job'"
 every :day, :at => '4:00am' do
   rake 'sync:lessons'
 end
-
-every :day, :at => '8:00am' do
- rake 'statistic:calculate'
-end
