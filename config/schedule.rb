e1633be7e970365eebdc03b1ad133aa36f740d5f
maximkuzmin <photographer.max@gmail.com>
2015-09-29 11:27:21 +0600

    statistic recalculation for last 31 days (or from semester_beginning) added to everyday schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,8 @@ set :job_template, "bash -c 'source ~/.rvm/environments/default && :job'"
 
 every :day, :at => '4:00am' do
   rake 'sync:lessons'
+end
+
+every :day, :at => '6:00am' do
   rake 'statistic:calculate_for_last_month'
 end
