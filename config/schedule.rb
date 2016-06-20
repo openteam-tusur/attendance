c4a54b7c3466004ef49f03ae02afcb103014c635
maximkuzmin <photographer.max@gmail.com>
2016-06-20 10:31:13 +0700

    sync is temporary off

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :job_template, "bash -c 'source ~/.rvm/environments/default && :job'"
 
-every :day, :at => '4:00am' do
-  rake 'sync:lessons'
-end
+#every :day, :at => '4:00am' do
+  #rake 'sync:lessons'
+#end
 
-every :day, :at => '6:00am' do
-  rake 'statistic:calculate_for_last_month'
-end
+#every :day, :at => '6:00am' do
+  #rake 'statistic:calculate_for_last_month'
+#end
