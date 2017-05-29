3c5ac8193b29817fabdd2a557677761bde5fcfaf
the_scrubs_fan <the_scrubs_fan@mail.ru>
2017-05-29 15:35:30 +0700

    added rest responser

---
+++ lib/catchers/rest_responser.rb
@@ -0,0 +1,16 @@
+module RestResponser
+  def self.rest_response(url)
+    res = RestClient::Request.execute(
+      method: :get,
+      url: url,
+      timeout: 120.seconds
+    ) do |response, request, result, &block|
+      response
+    end
+
+    {
+      code: res.code,
+      json: (JSON.load(res.body) rescue [])
+    }
+  end
+end
