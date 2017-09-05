cb031b75486afc3dc1382f96d50d69e4e8d95795
Evgeniy Lapin <lev@openteam.ru>
2017-09-05 15:15:20 +0700

    check if group of leader is a present

--- app/controllers/group_leader/lessons_controller.rb
+++ app/controllers/group_leader/lessons_controller.rb
@@ -7,11 +7,11 @@ class GroupLeader::LessonsController < AuthController
   actions :index
 
   def index
-    @group = current_user.leaded_groups.first
+    @group = current_user.leaded_groups.actual.first
     @date  = params[:by_date].try(:to_date) || today
     @calendar_months = (semester_begin..semester_end).group_by(&:month)
-    lessons = @group.lessons.actual.by_date(@date).order('order_number').includes(:realizes, :discipline)
+    lessons = @group.lessons.actual.by_date(@date).order('order_number').includes(:realizes, :discipline) rescue []
     @lessons_grouped_by_order = lessons.group_by(&:order_number)
-    @lessons_groupped_by_day = @group.lessons.actual.includes(:realizes, :presences).group_by(&:date_on)
+    @lessons_groupped_by_day = @group.lessons.actual.includes(:realizes, :presences).group_by(&:date_on) rescue []
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,18 +1,20 @@
 class Group < ActiveRecord::Base
   belongs_to :subdepartment
-  has_one    :faculty, :through => :subdepartment
-  has_many   :permissions,  :as => :context,          :dependent => :destroy
-  has_many   :memberships,  :as => :participate,      :dependent => :destroy
-  has_many   :students,     -> { where(:memberships => {:deleted_at => nil}).order('surname, name, patronymic') }, :through => :memberships, :source => :person, :source_type => 'Person', :class_name => 'Student'
-  has_many   :lessons,      :dependent => :destroy
-  has_many   :presences,    :through => :lessons
+  has_one :faculty, through: :subdepartment
+  has_many :permissions, as: :context, dependent: :destroy
+  has_many :memberships, as: :participate, dependent: :destroy
+  has_many :students, -> {
+    where(memberships: { deleted_at: nil }).order(:surname, :name, :patronymic)
+  }, through: :memberships, source: :person, source_type: 'Person', class_name: 'Student'
+  has_many :lessons, dependent: :destroy
+  has_many :presences, through: :lessons
 
   validates_presence_of :number
   normalize_attribute :number
 
-  scope :actual,      -> { where(:deleted_at => nil) }
-  scope :not_actual,  -> { where.not(:deleted_at => nil) }
-  scope :by_course,   -> (c) { where(:course => c) }
+  scope :actual, -> { where(:deleted_at => nil) }
+  scope :not_actual, -> { where.not(:deleted_at => nil) }
+  scope :by_course, -> (c) { where(:course => c) }
 
   alias_attribute :to_s, :number
 
@@ -21,15 +23,17 @@ class Group < ActiveRecord::Base
   end
 
   def students_at(date)
-    memberships.where("created_at < :date AND deleted_at IS NULL OR deleted_at > :date", :date => date).map(&:person).uniq.sort_by{|p| p[:surname]}
+    memberships.where('created_at < :date AND deleted_at IS NULL OR deleted_at > :date', date: date)
+      .map(&:person)
+      .uniq.sort_by{ |p| p[:surname] }
   end
 
   def group_leaders
-    permissions.where(:role => :group_leader).map(&:user).compact
+    permissions.where(role: :group_leader).map(&:user).compact
   end
 
   def group_leader
-    permission = permissions.where(:role => :group_leader).first
+    permission = permissions.where(role: :group_leader).first
     permission ? permission.user : 'нет старосты'
   end
 
