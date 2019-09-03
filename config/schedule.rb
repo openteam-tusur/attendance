5e2bea0c7d1c0cd4d60e00460cbc828674c18099
Evgeniy Lapin <lev@openteam.ru>
2019-09-04 05:17:53 +0700

    reverted: added warning message; disabled sync:lessons

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :job_template, "bash -c 'source ~/.rvm/environments/default && :job'"
 
 every :day, at: '2:00am' do
-  #rake 'sync:lessons'
+  rake 'sync:lessons'
 end
 
 every :day, at: '3:00am' do
