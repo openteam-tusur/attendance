10d02c2622dfe87b94eb94b6e7c91355933090e4
Evgeniy Lapin <lev@openteam.ru>
2020-09-15 19:09:48 +0700

    added links to lessons groups for filling by ED

--- app/controllers/education_department/filling/faculties_controller.rb
+++ app/controllers/education_department/filling/faculties_controller.rb
@@ -1,5 +1,4 @@
 class EducationDepartment::Filling::FacultiesController < AuthController
-
   def index
     @faculties = Faculty.actual.without_untracked
   end
@@ -8,5 +7,4 @@ class EducationDepartment::Filling::FacultiesController < AuthController
     @faculty = Faculty.includes(:groups).find_by(abbr: params[:id])
     @groups = @faculty.groups.actual.order(:course, :number).group_by(&:course)
   end
-
 end
