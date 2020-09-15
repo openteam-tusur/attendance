bd67a2cced623cde593186267a7b2c2c2bb0af5c
Evgeniy Lapin <lev@openteam.ru>
2020-09-15 17:20:57 +0700

    added stub faculty show with groups for filling by ED

--- app/controllers/education_department/filling/faculties_controller.rb
+++ app/controllers/education_department/filling/faculties_controller.rb
@@ -4,4 +4,9 @@ class EducationDepartment::Filling::FacultiesController < AuthController
     @faculties = Faculty.actual.without_untracked
   end
 
+  def show
+    @faculty = Faculty.includes(:groups).find_by(abbr: params[:id])
+    @groups = @faculty.groups.actual.order(:course, :number).group_by(&:course)
+  end
+
 end
